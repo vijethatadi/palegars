252a1786a4a4f6692d261186d1cbe9402eb028e4
Ankur Desai <ankurdesainew@gmail.com>
2016-06-23 14:32:25 -0400

    test commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name palegarsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the palegarsApp
+ */
+angular.module('palegarsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name palegarsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the palegarsApp
+ */
+angular.module('palegarsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
